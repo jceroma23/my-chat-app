6f3536780127c3bfe54c92d76e3d0a1725217cad
jceroma23 <jestceroma88@gmail.com>
2023-04-26 12:26:40 +0800

    Update Url Again

--- my-chat-app/src/utils/APIRoutes.js
+++ my-chat-app/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://my-chat-app-vuok-ni75lgtbh-jceroma23.vercel.app";
+export const host = "https://my-chat-app-vuok.vercel.app/";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
